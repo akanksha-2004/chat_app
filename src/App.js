4420e7c1f3c19ad018e605e4732ca743aa5a6fd4
Akanksha Shopra <akankshashopra@gmail.com>
2023-07-04 18:26:11 +0530

    make some components and pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import './styles/main.scss';
-import 'rsuite/dist/rsuite.default.css';
+import 'rsuite/dist/styles/rsuite-default.css';
+import { Routes } from 'react-router';
+import SignIn from './pages/SignIn';
+import PublicRoute from './components/PublicRoute';
 function App() {
-  return <div>hello</div>;
+  return (
+    <Routes>
+      <PublicRoute path="/signin">
+        <SignIn />
+      </PublicRoute>
+      <Privateroute path="/">
+        <Home />
+      </Privateroute>
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/misc/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase/app';
+const config = {
+  apiKey: 'AIzaSyBtZjbBVEhcr7ha-jNvunNk1Gw_hkGnGzw',
+  authDomain: 'chat-web-app-8bb69.firebaseapp.com',
+  projectId: 'chat-web-app-8bb69',
+  storageBucket: 'chat-web-app-8bb69.appspot.com',
+  messagingSenderId: '448068971713',
+  appId: '1:448068971713:web:57c9a67b46c376e2b93840',
+};
+
+const app = firebase.initializeApp(config);
